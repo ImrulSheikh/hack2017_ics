25b9c9845e1213872c841763512d5ad78f29d855
imrulh <imrulhasan@seekasia.com>
2017-12-12 16:36:07 +0800

    basic chat working

--- server.js
+++ server.js
@@ -4,15 +4,64 @@ import open from "open";
 const app = express();
 const port = 3000;
 
-app.get('/', function(req, res){
+app.get('/', function (req, res) {
     res.sendFile(__dirname + '/pub/index.html');
 });
 
-app.listen(port, function (err) {
-    console.log(`Example app listening on port ${port}`);
-    if (err) {
-        console.log(err);
-    } else {
-        open("http://localhost:" + port);
-    }
+
+
+
+// var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+
+
+io.on('connection', function (socket) {
+    console.log('a user connected');
+    socket.on('chat message', function (msg) {
+        console.log('message: ' + msg);
+    });
 });
+
+io.emit('some event', { for: 'everyone' });
+io.on('connection', function (socket) {
+    socket.broadcast.emit('hi');
+});
+
+io.on('connection', function (socket) {
+    socket.on('chat message', function (msg) {
+        io.emit('chat message', msg);
+    });
+});
+// io.on('connection', function(socket){
+//     console.log('a user connected');
+//     socket.on('disconnect', function(){
+//       console.log('user disconnected');
+//     });
+//   });
+
+
+http.listen(3000, function () {
+    console.log('listening on *:3000');
+});
+
+
+// app.listen(port, function (err) {
+//     console.log(`Example app listening on port ${port}`);
+//     if (err) {
+//         console.log(err);
+//     } else {
+//         open("http://localhost:" + port);
+//     }
+// });
+
+
+// app.listen(port, function (err) {
+//     console.log(`Example app listening on port ${port}`);
+//     if (err) {
+//         console.log(err);
+//     } else {
+//         open("http://localhost:" + port);
+//     }
+// });
